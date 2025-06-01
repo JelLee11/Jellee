4cfcac7ef534246df5973b0bbdaca6c8ce8435f6
JelLee <janamayeagtong13@gmail.com>
2025-06-01 10:19:55 +0800

    update src/routes/kokoroYakuRouter.js

--- src/routes/kokoroYakuRouter.js
+++ src/routes/kokoroYakuRouter.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const {
   getLatestUpdate,
+  getNovelInfo
 } = require("../controllers/kokoroYakuController");
 
 const router = express.Router();
 
 // Define the route for the newest novels
 router.get("/kokoro/latest", getLatestUpdate);
+// Route: /kokoro/info/:novelId
+router.get("/kokoro/info/:novelId", getNovelInfo);
 
 module.exports = router;
